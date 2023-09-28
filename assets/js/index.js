64623d0ba7364911ecbb14cc956dc47dc00ccc1c
Giancarlo Marques <gian_marques001@outlook.com>
2023-09-27 23:53:04 -0300

    Setimo Commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,49 @@
-const nome = document.querySelector("#nome");
-const outputNome = document.querySelector("#outNome");
+const qtdPags = document.querySelector("#qtdpags");
+const anoLivro = document.querySelector("#anolivro");
 
-const livroPercyJackson = document.querySelector("#percyjackson");
-const livroHarryPotter = document.querySelector("#harrypotter");
-const livroHobbit = document.querySelector("#hobbit");
+const tipoBasico = document.querySelector("#basica");
+const tipoMedio = document.querySelector("#media");
+const tipoCompleto = document.querySelector("#completa");
 
-const outputLivro = document.querySelector("#outLivro");
+const orcamento = document.querySelector("#outTipo");
 
-
-nome.addEventListener("input", function(){
-    outputNome.innerHTML = `Nome: ${nome.value}`
-})
-
-
-livroPercyJackson.addEventListener("change", function(){
-    outputLivro.innerHTML = `Livro Selecionado: Percy Jackson e o Ladrão de Raios`
+anoLivro.addEventListener("input", () => {
+  const ano = document.querySelector("label[for='anolivro']");
+  ano.innerHTML = `Ano do Livro: ${anoLivro.value} `;
+  calculo();
 });
 
-livroHarryPotter.addEventListener("change", function(){
-    outputLivro.innerHTML = `Livro Selecionado: Harry Potter e a Pedra Filosofal`
-});
+tipoBasico.addEventListener("change", calculo)
+tipoMedio.addEventListener("change", calculo)
+tipoCompleto.addEventListener("change", calculo)
 
-livroHobbit.addEventListener("change", function(){
-    outputLivro.innerHTML = `Livro Selecionado: O Hobbit`
-});
+qtdPags.addEventListener("change", calculo);
+
+function calculo() {
+  let qtd = qtdPags.value;
+  let valor = qtd * 0.4;
+  let tipo = 0;
+
+if (tipoBasico.checked){
+    tipo = 100;
+} 
+if (tipoMedio.checked){
+    tipo = 250;
+} 
+if (tipoCompleto.checked){
+    tipo = 500;
+} 
+
+
+  let taxa = anoLivro.value / 100;
+  if (anolivro.value > 1990 && anolivro.value < 2000) {
+    taxa = anolivro.value % 12;
+  } else if (anolivro.value >= 2000) {
+    taxa = anolivro.value % 8;
+  }
+valor += tipo;
+  valor *= taxa;
+
+
+  orcamento.innerHTML = `Orçamento: R$ ${valor.toFixed(0)},00`;
+}
